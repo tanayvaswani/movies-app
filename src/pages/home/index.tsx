23366be6b305ed1aa8789c18e5c344014db2d604
shivimishra <shivimishra130103@gmail.com>
2023-12-06 23:53:34 +0530

    home index

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from "semantic-ui-react";
+import { useState } from "react";
+
+enum DisplayType {
+    Movies = "movies",
+    TvShows = "tvshow",
+}
+
+export const Home = () => {
+    const [displayType, setDisplayType] = useState<DisplayType>(DisplayType.Movies)
+    return (
+        <div style={{ marginTop: 50, height: "auto" }}>
+            <Button.Group>
+                <Button color={displayType === DisplayType.Movies ? "blue" : undefined} 
+                onClick={() => setDisplayType(DisplayType.Movies)}>
+                    Movies
+                </Button>
+                <Button color={displayType === DisplayType.TvShows ? "blue" : undefined} 
+                onClick={() => setDisplayType(DisplayType.TvShows)}>
+                    TV Shows
+                </Button>
+            </Button.Group>
+        </div>
+    );
+};
