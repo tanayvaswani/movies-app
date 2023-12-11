cbccca96c938fae124b26d2079a1cdce05b795c2
shivimishra <shivimishra130103@gmail.com>
2023-12-11 22:06:58 +0530

    rating feature implemented

--- src/pages/home/column-display.tsx
+++ src/pages/home/column-display.tsx
@@ -1,55 +1,105 @@
 import { Link } from "react-router-dom";
 import { DisplayType } from ".";
-import { Card, Grid } from "semantic-ui-react";
+import { Card, Grid, Form } from "semantic-ui-react";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { rateMovie, rateTvShow } from "./mutation";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 interface DisplayData {
-    id: number;
-    overview: string;
-    poster_path: string;
-    title?: string;
-    name?: string;
-    vote_average: number;
-    release_date: string;
+  id: number;
+  overview: string;
+  poster_path: string;
+  title?: string;
+  name?: string;
+  vote_average: number;
+  release_date: string;
 }
 
 interface Props {
-    data: DisplayData[];
-    displayType: DisplayType;
+  data: DisplayData[];
+  displayType: DisplayType;
 }
 
 export const ColumnDisplay = (props: Props) => {
-    const { data, displayType } = props;
-    return (
-     <Grid
-        columns={3}
-        stackable
-        centered
-        verticalAlign="top"
-        padded="vertically"
-     >
-        {data.map((displayData: DisplayData) => (
-            <Grid.Column key={displayData.id}>
-                <Card.Group>
-                    <Link 
-                      to={`/${displayType === DisplayType.Movies ? "movie" : "tvshow"}
+  const { data, displayType } = props;
+  const [ rating, setRating ] = useState<number>(0);
+
+  const onSuccess = () => {
+    toast.success("Successfully rated!");
+  }
+
+  const onError = () => {
+    toast.error("Something went wrong!");
+  }
+
+  const { mutate: rateMovieMutation } = useMutation({
+    mutationKey: ["rateMovie"],
+    mutationFn: (id: number) => rateMovie(id, rating),
+    onSuccess: onSuccess,
+    onError: onError,
+  });
+
+  const { mutate: rateTvShowMutation } = useMutation({
+    mutationKey: ["rateTvShow"],
+    mutationFn: (id: number) => rateTvShow(id, rating),
+    onSuccess,
+    onError,
+  });
+
+  const rate = displayType=== DisplayType.Movies ? rateMovieMutation : rateTvShowMutation;
+
+  return (
+    <Grid
+      columns={3}
+      stackable
+      centered
+      verticalAlign="top"
+      padded="vertically"
+    >
+      {data.map((displayData: DisplayData) => (
+        <Grid.Column key={displayData.id}>
+          <Card.Group>
+            <Link
+              to={`/${displayType === DisplayType.Movies ? "movie" : "tvshow"}
                            /${displayData.id}`}
-                    >
-                        <Card 
-                            fluid 
-                            image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`} 
-                            header={
-                                displayType === DisplayType.Movies
-                                    ? displayData.title
-                                    : displayData.name
-                            } 
-                            meta={`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`} 
-                            description={displayData.overview.slice(0, 350) + "..."} 
-                        />
-                    </Link>
-                    
-                </Card.Group>
-            </Grid.Column>
-        ))}
-     </Grid> 
-    );
+            >
+              <Card
+                fluid
+                image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`}
+                header={
+                  displayType === DisplayType.Movies
+                    ? displayData.title
+                    : displayData.name
+                }
+                meta={`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`}
+                description={displayData.overview.slice(0, 350) + "..."}
+              />
+            </Link>
+            <Form style={{ marginTop: 10 }}>
+              <Form.Group inline>
+                <Form.Field>
+                  <Form.Input
+                    type="number"
+                    min="0"
+                    max="10"
+                    step="0.5"
+                    onChange={(e) => setRating(Number(e.target.value))}
+                    action={{
+                      color: "violet",
+                      labelPosition: "right",
+                      icon: "star",
+                      content: "Rate",
+                      onClick: () => rate(displayData.id),
+                    }}
+                  />
+                </Form.Field>
+              </Form.Group>
+            </Form>
+          </Card.Group>
+        </Grid.Column>
+      ))}
+    </Grid>
+  );
 };
---
+++ src/pages/home/mutation.ts
@@ -0,0 +1,38 @@
+
+export const rateMovie = async (movieId: number, rating: number) => {
+   
+       const res = await fetch(
+           `https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}&api_key=${import.meta.env.VITE_API_KEY}`,
+           {
+               method: "POST",
+               headers: {
+                    accept: "application/json",
+                    "content-type": "application/json;charset=utf-8",
+               },
+               body: `{"value": ${rating}}`,
+           }
+       );
+   
+       console.log(await res.json());              //res.json() returns a promise, so we should use await when logging the response.
+       return res.json();
+   }; 
+
+
+export const rateTvShow = async (tvShowId: number, rating: number) => {
+
+    const res = await fetch(
+        `https://api.themoviedb.org/3/tv/${tvShowId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}&api_key=${import.meta.env.VITE_API_KEY}`,
+        {
+            method: "POST",
+            headers: {
+                 accept: "application/json",
+                 "content-type": "application/json;charset=utf-8",
+            },
+            body: `{"value": ${rating}}`,
+        }
+    );
+
+   
+       console.log(await res.json());              
+       return res.json();
+   }; 
